0aecb7f1871b6569f4cf0f159effbf90a5ea0e84
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-01-27 16:46:12 +0100

    JSnack3 end

---
+++ JSnack2/js/main.js
@@ -0,0 +1,14 @@
+// Crea due tag div con due id diversi.Un div avrà il testo colorato di rosso mentre l’altro di verde.Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+const numbersList = [
+    1,
+    11,
+    42,
+    44,
+    53,
+    67,
+    77,
+    83,
+    86,
+    93
+]
+
